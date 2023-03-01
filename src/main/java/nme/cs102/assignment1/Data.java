27ce304c06575c7bef73de33e9d4a5f376a459ff
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-03-01 14:07:34 +0800

    cs209a lab2

---
+++ src/main/java/nme/cs102/assignment1/Data.java
@@ -0,0 +1,37 @@
+package nme.cs102.assignment1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Data {
+    public static void main(String[] args) {
+        long id;
+        float temp;
+        String PCR;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            id = sc.nextLong();
+            temp = sc.nextFloat();
+            PCR = sc.next();
+        }
+
+        List<String> errors = new ArrayList<>();
+
+        if (String.valueOf(id).length() != 8) {
+            errors.add(" Student ID");
+        }
+        if(temp <= 35.0 || temp >= 45.0) {
+            errors.add(" Temperature");
+        }
+        if (PCR.length() != 1 || !(PCR.equals("Y") || PCR.equals("N"))) {
+            errors.add(" Nucleic Acid PCR test");
+        }
+
+        if(errors.isEmpty()) {
+            System.out.print("Submit successfully");
+        } else {
+            System.out.printf("Error in%s", String.join(" and", errors.toArray(new String[0])));
+        }
+    }
+}
